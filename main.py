a7f43804e384478cd72c1d750a3466db4c8bf319
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2017-10-08 14:44:22 +0200

    removes  option

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler('verify', cmd_verify, pass_args=True))
     dispatcher.add_handler(CommandHandler('export_friends', cmd_export_friends))
     dispatcher.add_handler(CommandHandler('set_timezone', cmd_set_timezone, pass_args=True))
-    dispatcher.add_handler(MessageHandler([Filters.text], handle_chat))
+    #dispatcher.add_handler(MessageHandler([Filters.text], handle_chat))
 
     # put job
     queue = updater.job_queue
