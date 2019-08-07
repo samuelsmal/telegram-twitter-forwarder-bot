8f175d424f453055749d6e54337cc4515512fd34
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2019-08-07 23:03:24 +0200

    fixes message?

--- commands.py
+++ commands.py
@@ -198,7 +198,7 @@ def cmd_source(bot, update, chat=None):
     bot.reply(update, "This bot is Free Software under the LGPLv3. "
                     "You can get the code from here: "
                     "https://github.com/samuelsmal/telegram-twitter-forwarder-bot/"
-                    "(it's a fork from https://github.com/franciscod/telegram-twitter-forwarder-bot")
+                    " (it's a fork from https://github.com/franciscod/telegram-twitter-forwarder-bot)")
 
 
 @with_touched_chat
